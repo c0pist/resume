14f72048c4c295f7ce0a9e0165177330a8023cf5
c0pist <42822834+c0pist@users.noreply.github.com>
2020-03-26 12:01:43 -0500

    update

---
+++ src/pages/Channel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Channel = () => {
+    return (
+        <div>
+            Channel
+        </div>
+    );
+};
+export default Channel;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             Home
+            <a href="/Channel">Channel</a>
         </div>
     );
 };
