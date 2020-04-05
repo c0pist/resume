07049404a73c95923c24ea81c259c840423624e0
c0pist <42822834+c0pist@users.noreply.github.com>
2020-04-05 14:18:22 -0500

    d

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-//import { Home, Channel, More } from '../pages';
-import Home from '../pages/Home';
-import Channel from '../pages/Channel';
-import More from '../pages/More';
+import { Home, Channel, More } from '../pages';
 
 class App extends Component {
   render() {
     return (
-      <router>
         <div>
           <Route exact path="/" Component={Home}/>
           <Route path="/channel" Component={Channel}/>
           <Route path="/more" Component={More}/>
         </div>
-      </router>
     );
   }
 }
