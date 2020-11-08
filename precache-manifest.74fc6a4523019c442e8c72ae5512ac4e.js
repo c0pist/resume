ee67d661bde1d745036bbdace5d248c1ecddb579
c0pist <c0pist@hotmail.com>
2020-11-08 13:21:06 -0500

    Updates

---
+++ precache-manifest.74fc6a4523019c442e8c72ae5512ac4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d79c5b11ff7f1456c30a8fedf1e881",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "b1fc63ab9f65da6a1b4b",
+    "url": "/blog/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "ca1e83922637ff33c97a",
+    "url": "/blog/static/css/main.1aadbe2c.chunk.css"
+  },
+  {
+    "revision": "b1fc63ab9f65da6a1b4b",
+    "url": "/blog/static/js/2.b4318f69.chunk.js"
+  },
+  {
+    "revision": "3d99149c13dce31e358d86e393036a3c",
+    "url": "/blog/static/js/2.b4318f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1e83922637ff33c97a",
+    "url": "/blog/static/js/main.3ea263f5.chunk.js"
+  },
+  {
+    "revision": "042e0e1222fb6f3edc08",
+    "url": "/blog/static/js/runtime-main.1dc37962.js"
+  }
+]);
